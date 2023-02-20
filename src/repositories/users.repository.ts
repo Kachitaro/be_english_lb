38c683d4220516a87e46660792fd311439e72e7b
Kachitaro <anhtai741981@gmail.com>
2023-02-20 17:32:37 +0700

    oke model không lỗi hoàn hảo chạy được migrate

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -1,27 +1,16 @@
-import { Getter, inject } from '@loopback/core';
-import { BelongsToAccessor, DefaultCrudRepository, repository } from '@loopback/repository';
+import { inject } from '@loopback/core';
+import { DefaultCrudRepository } from '@loopback/repository';
 import { PgDataSource } from '../datasources';
-import { Manager, Student, Users, UsersRelations } from '../models';
-import { ManagerRepository } from './manager.repository';
-import { StudentRepository } from './student.repository';
+import {Users, UsersRelations} from '../models/users.model';
 
 export class UsersRepository extends DefaultCrudRepository<
   Users,
   typeof Users.prototype.id,
   UsersRelations
 > {
-
-  public readonly student: BelongsToAccessor<Student, typeof Users.prototype.id>;
-
-  public readonly manager: BelongsToAccessor<Manager, typeof Users.prototype.id>;
-
   constructor(
-    @inject('datasources.pg') dataSource: PgDataSource, @repository.getter('StudentRepository') protected studentRepositoryGetter: Getter<StudentRepository>, @repository.getter('ManagerRepository') protected managerRepositoryGetter: Getter<ManagerRepository>,
+    @inject('datasources.pg') dataSource: PgDataSource,
   ) {
     super(Users, dataSource);
-    this.manager = this.createBelongsToAccessorFor('manager', managerRepositoryGetter,);
-    this.registerInclusionResolver('manager', this.manager.inclusionResolver);
-    this.student = this.createBelongsToAccessorFor('student', studentRepositoryGetter,);
-    this.registerInclusionResolver('student', this.student.inclusionResolver);
   }
 }
